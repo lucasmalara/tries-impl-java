184a3e8e1db4a4d30090c213a3743c50838925b2
Lucas <malara.career.d9mwd@slmail.me>
2023-12-12 13:47:33 +0100

    Init project.

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "com.lucasmalara.datastruct.trie"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("--enable-preview")
+}
